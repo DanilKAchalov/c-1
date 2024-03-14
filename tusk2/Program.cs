b613297f6d1b296c5f97ac16a0be5c1b43734f7c
Danil <Danya.kocha@bk.ru>
2024-03-14 16:19:36 +0300

    Добавили пару строк кода с вычислениями

---
+++ tusk2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int a1 = 5;
+int a2 = 7;
+int Sum = a1 + a2;
+Console.WriteLine(Sum);
+
+long l1 = 6;
+long Sum2 = a1 + l1;
+Console.WriteLine(Sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + Sum;
+Console.WriteLine(sum3);
+
+
+int prd1 = a1 * a2;
+long prd2 = l1 * a1;
+double prd3 = a1 * d1;
+Console.WriteLine(prd1);
+Console.WriteLine(prd2);
+Console.WriteLine(prd3);
